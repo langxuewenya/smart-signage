4ed259399d4915f3f18605b05054580fc3f0c3e6
langxuewenya <langxuewenya@163.com>
2025-10-21 18:40:14 +0800

    feat: 界面联调

--- src/router/modules/home.ts
+++ src/router/modules/home.ts
@@ -1,11 +1,24 @@
 const { VITE_HIDE_HOME } = import.meta.env;
 const Layout = () => import("@/layout/index.vue");
+import { storageLocal } from "@pureadmin/utils";
+import { type DataInfo, userKey } from "@/utils/auth";
+
+const redirectUrl = () => {
+  const roles = storageLocal().getItem<DataInfo<number>>(userKey)?.roles;
+  console.log(roles);
+  if (roles.includes("admin")) {
+    return "/user-mgr";
+  } else {
+    return "/device-class";
+  }
+};
 
 export default {
   path: "/",
   name: "Home",
   component: Layout,
-  redirect: "/welcome",
+  // redirect: "/welcome",
+  redirect: redirectUrl(),
   meta: {
     icon: "ep/home-filled",
     title: "首页",
